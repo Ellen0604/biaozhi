dff8acb437c63797c8f8db0312c8044150dacd6e
Ellen0604 <Ellen0604@aliyun.com>
2018-05-14 20:03:43 +0800

    1

--- js/index.js
+++ js/index.js
@@ -20,6 +20,7 @@
 			freeMode: true,
 			freeModeMomentum: false,
 			freeModeMomentumBounce: false,
+			freeModeMinimumVelocity : 1,
 			nested: true,
 		});
 
@@ -105,6 +106,50 @@
 			}
 		}
 	}
+
+	function WXShare (data,title,img,desc){
+    	wx.config({
+		    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+		    appId: data.appId, // 必填，公众号的唯一标识
+		    timestamp: data.timestamp , // 必填，生成签名的时间戳
+		    nonceStr: data.nonceStr, // 必填，生成签名的随机串
+		    signature: data.signature,// 必填，签名，见附录1
+		    jsApiList: ['onMenuShareTimeline','onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+		});
+
+		wx.ready(function () {
+			console.log(title,img,desc)
+			var link=decodeURIComponent(location.href.split('#')[0]);
+			var ShareTitle=title?title:"51来战";
+			var ShareImg=img?img:"http://test.51laizhan.com/user/imgs/lz_logo1.png";
+			var ShareDesc=desc?desc:"来战-电竞圈社交约战平台";
+			// 分享到朋友圈
+		    wx.onMenuShareTimeline({
+		    	title: ShareTitle, // 分享标题
+			    link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+			    imgUrl: ShareImg, // 分享图标
+			    success: function () { 
+			        // 用户确认分享后执行的回调函数
+			    },
+			    cancel: function () { 
+			        // 用户取消分享后执行的回调函数
+			    }
+		    })
+		    // 分享给朋友
+		    wx.onMenuShareAppMessage({
+				title: ShareTitle, // 分享标题
+			    desc: ShareDesc, // 分享描述
+			    link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+			    imgUrl: ShareImg, // 分享图标
+			    success: function () { 
+			        // 用户确认分享后执行的回调函数
+			    },
+			    cancel: function () { 
+			        // 用户取消分享后执行的回调函数
+			    }
+		    });
+		});
+    }
 	
 	$(function(){
 		/*按屏幕修改根文本尺寸*/
